512df226d2fad0ff3e76e960c0c504f68b978dc0
LONTERRA <lukeonyebuchi77@gmail.com>
2022-09-17 02:55:18 -0700

    Print_alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,14 @@
 #define MAIN_H
 int _putchar(char c);
 void printMul(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int print_sign(int n);
+int print_last_digit(int);
+void times_table(void);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int n);
 
 #endif /* MAIN_H */
