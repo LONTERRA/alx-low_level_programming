b88908f53484a22bb5f5c06fa42eeebffc6fc585
LONTERRA <lukeonyebuchi77@gmail.com>
2022-09-17 23:25:30 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,11 @@
 
 /**
  * print  _islower - checks for lowercase character
- *
  * @c: the character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
+
 }
